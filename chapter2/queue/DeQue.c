b82cef5c47281a8dff599ceb6bb12f7dbfacdab9
gehan <myxy_hg@163.com>
2017-04-17 05:29:24 -0700

    commit for queue structure

---
+++ chapter2/queue/DeQue.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int UINT;
+
+/* the definition of circuit queue structure in DeQue */
+typedef struct DEQUEBLOCK_st
+{
+    UINT uHead;
+    UINT uTail;
+    UINT uMapPos;   /* position in map */
+    void **ppData;
+}DEQUEBLOCK;
+
+/* the definition of DeQue */
+typedef struct DEQUE_st
+{
+    DEQUEBLOCK **ppMap;     /* the pointer of map array */
+    DEQUEBLOCK *pFirst;     /* the first pointer to circuit queue */
+    DEQUEBLOCK *pLast;
+    UINT uMapSize;
+    UINT uBlockSize;        /* the size of each circuit queue */
+}DEQUE;
+
+/*
+ * the constructure of circuit queue in DeQue
+ * @param UINT uBlockSize
+ * @param DEQUEBLOCK * -- the pointer to circuit queue
+ */
+DEQUEBLOCK *DeQueBlock_Create(UINT uBlockSize)
+{
+    DEQUEBLOCK *pBlock;
+    pBlock = (DEQUEBLOCK *)malloc(sizeof(DEQUEBLOCK));
+    if(pBlock != NULL)
+    {
+        pBlock->ppData = (void **)malloc(uBlockSize * sizeof(void *));
+        if(pBlock->ppData == NULL)
+        {
+            free(pBlock);
+            return NULL;
+        }
+        pBlock->uHead = 0;
+        pBlock->uTail = 0;
+    }
+    return pBlock;
+}
+
+/*
+ * DeQue's function for insert data in tail
+ * @param DEQUE *pQue -- the pointer to DeQue
+ * @param void *pData
+ * @return INT
+ */
+INT DeQue_InsertTail(DEQUE *pQue, void *pData)
+{
+    DEQUEBLOCK *pBlock;
+    UINT uTailNext;
+    pBlock = pQue->pLast;
+    if(pBlock->uTail == pQue->uBlockSize-1)
+        uTailNext = 0;
+    else
+        uTailNext = pBlock->uTail+1;
+
+    /* the queue is not full */
+    if(pBlock->uHead != uTailNext)
+    {
+        pBlock->ppData[pBlock->uTail] = pData;
+        pBlock->uTail = uTailNext;
+    }
+    else
+    {
+        DEQUEBLOCK *pNewBlock = DeQueBlock_Create(pQue->uBlockSize);
+        if(pNewBlock == NULL)
+            return CAPI_FAILED;
+
+        if(pBlock->uMapPos >= pQue->uMapSize-1)
+        {
+            /* reallocate map */
+            DEQUEBLOCK **ppMap = (DEQUEBLOCK **)malloc(pQue->uMapSize * 2 * sizeof(DEQUEBLOCK *));
+            if(ppMap == NULL)
+                return CAPI_FAILED;
+
+            memcpy(ppMap,pQue->ppMap,pQue->uMapSize*sizeof(DEQUEBLOCK *));
+            pQue->uMapSize *= 2;
+            free(pQue->ppMap);
+            pQue->ppMap = ppMap;
+        }
+        pNewBlock->uMapPos = pBlock->uMapPos + 1;
+        pNewBlock->ppData[0] = pData;
+        pNewBlock->uTail += 1;
+        pQue->ppMap[pNewBlock->uMapPos] = pNewBlock;
+        pQue->pLast = pNewBlock;
+    }
+    return CAPI_SUCCESS;
+}
+
+/*
+ * pop head data function of DeQue
+ * @param DEQUE *pQue
+ * @return void *
+ */
+void *DeQue_PopHead(DEQUE *pQue)
+{
+    DEQUEBLOCK *pBlock;
+    UINT uHead;
+    pBlock = pQue->pFirst;
+    uHead = pBlock->uHead;
+    if(uHead != pBlock->uTail)
+    {
+        if(uHead == pQue->uBlockSize-1)
+            pBlock->uHead = 0;
+        else
+            pBlock->uHead += 1;
+
+        if(pBlock->uHead == pBlock->uTail)
+        {
+            if(pQue->pLast != pQue->pFirst)
+            {
+                UINT uMapPos = pBlock->uMapPos;
+                DeQueBlock_Destroy(pQue,pBlock,NULL);
+                pBlock = pQue->ppMap[uMapPos+1];
+                pQue->pFirst = pBlock;
+            }
+        }
+        return pBlock->ppData[uHead];
+    }
+    else
+        return NULL;
+}
---
+++ chapter2/queue/queue.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned int UINT;
+typedef int INT;
+
+typedef struct QUEUE_st{
+    void **ppData;      /* the pointer array for saving data pointer */
+    UINT uMaxCount;     /* the maximum quantity in queue */
+    UINT uHead;
+    UINT uTail;
+}QUEUE;
+
+/*
+ * insert data to queue's tail, double size if queue is full
+ * @param QUEUE *pQueue
+ * @param void *pData
+ * @return INT -- return CAPI_FAILED means can not apply for memory
+ * and insertion operation was failed; return CAPi_SUCCESS means
+ * insert successfully.
+ */
+INT Queue_InsertTail(QUEUE *pQueue, void *pData)
+{
+    UINT uTailNext;
+    /* get next location after tail */
+    if(pQueue->uTail == pQueue->uMaxCount-1)
+    {
+        /*
+         * next location need to be counted from array's head
+         * when pointer arrival array's tail
+         */
+        uTailNext = 0;
+    }
+    else
+        uTailNext = pQueue->uTail + 1;
+
+    /* the queue is not full */
+    if(uTailNext != pQueue->uHead)
+    {
+        pQueue->ppData[pQueue->uTail] = pData;
+        pQueue->uTail = uTailNext;
+    }
+    /* queue is full and double size */
+    else
+    {
+        void **ppData = (void **)malloc(pQueue->uMaxCount * 2 * sizeof(void *));
+        if(ppData == NULL)
+            return CAPI_FAILED;
+        if(pQueue->uHead > pQueue->Tail)
+        {
+            UINT i;
+            /* copy data from uHead to queue's tail */
+            for(i = pQueue->uHead;i < pQueue->uMaxCount; i++)
+                ppData[i] = pQueue->ppData[i];
+            /* copy data from 0 to uTail */
+            for(i = 0; i < pQueue->uTail; i++)
+                ppData[i+pQueue->uMaxCount] = pQueue->ppData[i];
+            pQueue->uTail += pQueue->uMaxCount;
+        }
+        else
+        {
+            UINT i;
+            for(i = pQueue->uHead;i < pQueue->uTail;i++)
+                ppData[i] = pQueue->ppData[i];
+        }
+    /* insert data into new allocate memory */
+    ppData[pQueue->uTail] = pData;
+    pQueue->uTail += 1;
+    pQueue->uMaxCount *= 2;
+    free(pQueue->ppData);
+    pQueue->ppData = ppData;
+    }
+    return CAPI_SUCCESS;
+}
+
+/*
+ * pop head's data in queue
+ * @param QUEUE *pQueue
+ * @return void *
+ */
+void *Queue_PopHead(QUEUE *pQueue)
+{
+    UINT uHead;
+    uHead = pQueue->uHead;
+    if(uHead != pQueue->uTail)
+    {
+        if(uHead == pQueue->uMaxCount-1)
+            pQueue->uHead = 0;
+        else
+            pQueue->uHead += 1;
+        return pQueue->ppData[uHead];
+    }
+    else
+        return NULL;
+}
