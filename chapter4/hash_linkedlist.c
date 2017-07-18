8750ea3d73ea8df0de129a6b991ed3eef4ae11c7
myxyhg <myxy_hg@163.com>
2017-07-18 21:41:52 +0800

    Complete hash list's implementation

--- chapter4/hash_linkedlist.c
+++ chapter4/hash_linkedlist.c
@@ -91,3 +91,277 @@ void HashList_Destroy(HASHLIST *pHashList, DESTROYFUNC DestroyFunc)
 	free(pHashList);
 	return;
 }
+
+/*
+ * To insert node into hash list, and must be in head of linked list
+ * @param HASHLIST *pHashList
+ * @param void *pData
+ * @param HASHFUNC HashFunc
+ * @return INT
+ */
+INT HashList_InsertHead(HASHLIST *pHashList, void *pData, HASHFUNC HashFunc)
+{
+	HASHLISTNODE *pNode;
+	UINT uBucketIndex;
+	if (NULL == pHashList || NULL == pData)
+	{
+		return CAPI_FAILED;
+	}
+
+	pNode = (HASHLISTNODE *)malloc(sizeof(HASHLISTNODE));
+	if (NULL == pNode)
+	{
+		return CAPI_FAILED;
+	}
+	pNode->pData = pData;
+	pNode->pBucketNext = NULL;
+	pNode->pListPrev = NULL;
+	pNode->pListNext = pHashList->pHead;
+
+	/*Insert into hash list*/
+	uBucketIndex = (*HashFunc)(pData, pHashList->uBucketCount);
+	if (NULL == pHashList->ppBuckets[uBucketIndex])
+	{
+		pHashList->ppBuckets[uBucketIndex] = pNode;
+	}
+	else
+	{
+		HASHLISTNODE *pTempNode;
+		pTempNode = pHashList->ppBuckets[uBucketIndex];
+		while (NULL != pTempNode->pBucketNext)
+		{
+			pTempNode = pTempNode->pBucketNext;
+		}
+		pTempNode->pBucketNext = pNode;
+	}
+
+	/*Insert into linkedlist*/
+	if (NULL == pHashList->pHead)
+	{
+		pHashList->pHead = pNode;
+		pHashList->pTail = pNode;
+	}
+	else
+	{
+		pNode->pListNext = pHashList->pHead;
+		pHashList->pHead->pListPrev = pNode;
+		pHashList->pHead = pNode;
+	}
+	pHashList->uNodeCount += 1;
+	return CAPI_SUCCESS;
+}
+
+/*
+ * To delete node in hash list, delete it from hash table and linkedlist at same time
+ * @param HASHLIST *pHashList
+ * @param void *pData
+ * @param HASHFUNC HashFunc
+ * @param COMPAREFUNC CompareFunc
+ * @param DESTROYFUNC DestroyFunc
+ * @return INT
+ */
+INT HashList_Delete(HASHLIST *pHashList, void *pData, HASHFUNC HashFunc,
+	COMPAREFUNC CompareFunc, DESTROYFUNC DestroyFunc)
+{
+	HASHLISTNODE *pNode, *pPrevNode;
+	UINT uIndex;
+	if (NULL == pHashList || NULL == HashFunc || NULL == CompareFunc)
+	{
+		return CAPI_FAILED;
+	}
+
+	uIndex = (*HashFunc)(pData, pHashList->uBucketCount);
+	pNode = pHashList->ppBuckets[uIndex];
+	pPrevNode = NULL;
+	while (NULL != pNode)
+	{
+		if (0 == (*CompareFunc)(pNode->pData, pData))
+		{
+			if (NULL == pPrevNode)
+			{
+				pHashList->ppBuckets[uIndex] = pNode->pBucketNext;
+			}
+			else
+			{
+				pPrevNode->pBucketNext = pNode->pBucketNext;
+			}
+
+			/*Delete node from linked list*/
+			if (NULL != pNode->pListPrev)
+			{
+				pNode->pListPrev->pListNext = pNode->pListNext;
+			}
+			else
+			{
+				/*pNode is the head of linked list*/
+				pHashList->pHead = pNode->pListNext;
+			}
+
+			if (NULL != pNode->pListNext)
+			{
+				pNode->pListNext->pListPrev = pNode->pListPrev;
+			}
+			else
+			{
+				/*Now at tail of linked list*/
+				pHashList->pTail = pNode;
+			}
+
+			if (NULL != pNode->pData && NULL != DestroyFunc)
+			{
+				(*DestroyFunc)(pNode->pData);
+			}
+			free(pNode);
+			pHashList->uNodeCount -= 1;
+			return CAPI_SUCCESS;
+		}
+		else
+		{
+			pPrevNode = pNode;
+			pNode = pNode->pBucketNext;
+		}
+	}
+	return CAPI_FAILED;
+}
+
+/*
+ * To find node in hash list
+ * @param HASHLIST *pHashList
+ * @param void *pData
+ * @param HASHFUNC HashFunc
+ * @param COMPAREFUNC CompareFunc
+ * @return HASHLISTNODE *
+ */
+HASHLISTNODE * HashList_FindNode(HASHLIST *pHashList, void *pData,
+	HASHFUNC HashFunc, COMPAREFUNC CompareFunc)
+{
+	HASHLISTNODE *pNode;
+	UINT uIndex;
+	if (NULL == pHashList || NULL == HashFunc || NULL == CompareFunc)
+	{
+		return NULL;
+	}
+
+	uIndex = (*HashFunc)(pData, pHashList->uBucketCount);
+	pNode = pHashList->ppBuckets[uIndex];
+	while (NULL != pNode)
+	{
+		if (0 == (*CompareFunc)(pNode->pData, pData))
+		{
+			return pNode;
+		}
+		else
+		{
+			pNode = pNode->pBucketNext;
+		}
+	}
+	return NULL;
+}
+
+/*
+ * To find data in hash list
+ * @param HASHLIST *pHashList
+ * @param void *pData
+ * @param HASHFUNC HashFunc
+ * @param COMPAREFUNC CompareFunc
+ * @return void *
+ */
+HASHLISTNODE * HashList_FindData(HASHLIST *pHashList, void *pData,
+	HASHFUNC HashFunc, COMPAREFUNC CompareFunc)
+{
+	HASHLISTNODE *pNode;
+	UINT uIndex;
+	if (NULL == pHashList || NULL == HashFunc || NULL == CompareFunc)
+	{
+		return NULL;
+	}
+
+	uIndex = (*HashFunc)(pData, pHashList->uBucketCount);
+	pNode = pHashList->ppBuckets[uIndex];
+	while (NULL != pNode)
+	{
+		if (0 == (*CompareFunc)(pNode->pData, pData))
+		{
+			return pNode->pData;
+		}
+		else
+		{
+			pNode = pNode->pBucketNext;
+		}
+	}
+	return NULL;
+}
+
+/*
+ * The insert sort function for hash list
+ * @param HASHLIST *pHashList
+ * @param COMPAREFUNC CompareFunc
+ * @return INT
+ */
+INT HashList_InsertSort(HASHLIST *pHashList, COMPAREFUNC CompareFunc)
+{
+	HASHLISTNODE *pNode;
+	if (NULL == pHashList || NULL == CompareFunc)
+	{
+		return CAPI_FAILED;
+	}
+
+	pNode = pHashList->pHead;
+	if (NULL == pNode)
+	{
+		/*No node in hash list, it already sorted*/
+		return CAPI_SUCCESS;
+	}
+
+	while (NULL != pNode->pListNext)
+	{
+		if ((*CompareFunc)(pNode->pListNext->pData, pNode->pData) < 0)
+		{
+			HASHLISTNODE *pTempNode;
+			pTempNode = pNode->pListPrev;
+			while (NULL != pTempNode)
+			{
+				if ((*CompareFunc)(pNode->pListNext->pData, pTempNode->pData) >= 0)
+				{
+					HASHLISTNODE *pCurNode;
+					pCurNode = pNode->pListNext;
+					pNode->pListNext = pNode->pListNext->pListNext;
+					if (NULL != pCurNode->pListNext)
+					{
+						pCurNode->pListNext->pListPrev = pNode;
+					}
+					pCurNode->pListNext = pTempNode->pListNext;
+					pCurNode->pListPrev = pTempNode;
+					pTempNode->pListNext->pListPrev = pCurNode;
+					pTempNode->pListNext = pCurNode;
+					break;
+				}
+				else
+				{
+					pTempNode = pTempNode->pListPrev;
+				}
+
+				/*If all data is greater than this node's data, then insert it to head*/
+				if (NULL == pTempNode)
+				{
+					HASHLISTNODE *pCurNode;
+					pCurNode = pNode->pListNext;
+					pNode->pListNext = pNode->pListNext->pListNext;
+					if (NULL != pCurNode->pListNext)
+					{
+						pCurNode->pListNext->pListPrev = pNode;
+					}
+					/*Insert node into head*/
+					pCurNode->pListPrev = NULL;
+					pCurNode->pListNext = pHashList->pHead;
+					pHashList->pHead = pCurNode;
+				}
+			}
+		}
+		else
+		{
+			pNode = pNode->pListNext;
+		}
+	}
+	return CAPI_SUCCESS;
+}
